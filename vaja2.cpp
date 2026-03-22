329590439616ac754ab7c156b5fdae63f7a6f9a0
lebargasper <gasper.lebar@studennt.um.si>
2026-03-22 11:58:54 +0100

    Dodal funkcionalnost za funkcijo Branje_Stevil

--- vaja2.cpp
+++ vaja2.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 bool Branje_Stevil(vector<unsigned char> &vec, const char s[]) {
+    ifstream input(s);
+	int st;
+
+	if (!input.is_open()) {
+		return false;
+	}
+
+	while (input >> st) {
+        if (st < 0 || st > 255) return false;
+        vec.push_back(static_cast<unsigned char>(st));
+    }
+	input.close();
+	return true;
 }
 
 int main() {
